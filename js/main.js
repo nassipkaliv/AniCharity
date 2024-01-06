83f3cae4b748274a3ef693cb85428ac657286827
nassipkaliv <nassipkaliv@gmail.com>
2024-01-07 00:09:56 +0600

    js updates!

--- js/main.js
+++ js/main.js
@@ -23,3 +23,38 @@ $(document).ready(function() {
       $('.arrow-icon').toggleClass('rotate');
   });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var playButton = document.querySelector('.btn-play');
+
+  playButton.addEventListener('click', function() {
+      window.location.href = 'https://www.youtube.com/watch?v=4KT9BN-MSkM&ab_channel=%D0%9F%D0%BB%D0%B0%D0%BD%D0%B5%D1%82%D0%B0%D0%97%D0%B5%D0%BC%D0%BB%D1%8F';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var collapseElement = document.getElementById('moreAboutShefstvo');
+  var toggleTextSpan = document.querySelector('.more-shefstvo .shefstvo-toggle-text');
+  var toggleIcon = document.querySelector('.more-shefstvo .shefstvo-toggle-icon');
+
+  // Function to update text and icon on collapse show/hide
+  function updateToggleElement(isCollapsed) {
+      if (isCollapsed) {
+          toggleTextSpan.textContent = 'Свернуть';
+          toggleIcon.style.transform = 'rotate(180deg)';
+      } else {
+          toggleTextSpan.textContent = 'Подробнее о шефстве';
+          toggleIcon.style.transform = 'rotate(0deg)';
+      }
+  }
+
+  // Event listener for when the collapse element is shown
+  collapseElement.addEventListener('show.bs.collapse', function() {
+      updateToggleElement(true);
+  });
+
+  // Event listener for when the collapse element is hidden
+  collapseElement.addEventListener('hide.bs.collapse', function() {
+      updateToggleElement(false);
+  });
+});
